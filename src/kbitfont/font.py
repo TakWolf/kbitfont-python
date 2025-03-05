42f3f91b646da1280150655718375eac461bb319
TakWolf <takwolf@foxmail.com>
2025-03-05 20:39:07 +0800

    基本类型定义

--- src/kbitfont/font.py
+++ src/kbitfont/font.py
@@ -1,4 +1,18 @@
+from kbitfont.glyph import KbitGlyph
+from kbitfont.names import KbitNames
+from kbitfont.props import KbitProps
+
 
 class KbitFont:
+    props: KbitProps
+    names: KbitNames
+    characters: dict[int, KbitGlyph]
+    named_glyphs: dict[str, KbitGlyph]
+    kern_pairs: dict[tuple[int | str, int | str], int]
+
     def __init__(self):
-        pass
+        self.props = KbitProps()
+        self.names = KbitNames()
+        self.characters = {}
+        self.named_glyphs = {}
+        self.kern_pairs = {}
